7a4d242f2a01a52937b66963bc4993ee5fe34bd0
Varun Choudhary <143994029+varruunnn@users.noreply.github.com>
2025-01-16 18:54:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://realtimeeditor-c36r.onrender.com',
+        ws: true, 
+        changeOrigin: true,
+      },
+    },
+  },
+});
